8a40f63a603e10a4373ffc9df29cfcb584a7258e
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-07 20:20:37 +0200

    add validation to task and user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,31 +1,43 @@
 const mongoose = require("mongoose");
-const validator =require('validator')
+const validator = require("validator");
 //connection
 mongoose.connect("mongodb://127.0.0.1:27017/task-manger-api");
 
 //model (collection as object)
 const User = mongoose.model("user", {
-  name: { type: String },
-  email:{type:String,validate(value){
-    if(! validator.isEmail(value)){
-      throw new error ('invalid email')
-    }
-  }},
-  age: { type: Number , validate(value){
-    if(value<0){
-      throw new error ('negative age')   }
-      
-  }},
- 
-});
+  name: { type: String, required: true, trim: true },
+  password: {
+    type: String, required: true, trim: true,
+     validate(value) { if (value.includes('password'))
+       { throw new error('invalid password') } }},
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new error("invalid email");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 18,
+      validate(value) {
+        if (value < 0) {
+          throw new error("negative age");
+        }
+      },
+    },
+  });
 
 const task = mongoose.model("task", {
-  description: { type: String, required: true },
-  completed: { type: Boolean },
+  description: { type: String, required: true ,trim:true},
+  completed: { type: Boolean ,default:false },
 });
 
 //use model
-const me =new User({name:'mo',email:'mo@gmail.com',age:24})
+const me = new User({ name: "ahmed", password:'asd',email: "Mo@gmail.com", age: 24 });
 //const task1 = new task({ description: "Clean the house", completed: true });
 
 /*
@@ -38,4 +50,10 @@ task1
     console.log(error);
   });
   */
-me.save().then((res)=>{console.log(res);}).catch((error)=>{console.log(error);})
+me.save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
