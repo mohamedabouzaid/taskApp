7b4ef17c0c1a123f25cdc77541d9f03f2f813399
abouzaid <mohmmdabouzaid@gmail.com>
2022-03-07 18:52:09 +0200

    add validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
-
+const validator =require('validator')
 //connection
 mongoose.connect("mongodb://127.0.0.1:27017/task-manger-api");
 
 //model (collection as object)
 const User = mongoose.model("user", {
   name: { type: String },
-  age: { type: Number },
+  email:{type:String,validate(value){
+    if(! validator.isEmail(value)){
+      throw new error ('invalid email')
+    }
+  }},
+  age: { type: Number , validate(value){
+    if(value<0){
+      throw new error ('negative age')   }
+      
+  }},
+ 
 });
 
 const task = mongoose.model("task", {
@@ -15,9 +25,10 @@ const task = mongoose.model("task", {
 });
 
 //use model
-//const me =new User({name:'mo',age:27})
-const task1 = new task({ description: "Clean the house", completed: true });
+const me =new User({name:'mo',email:'mo@gmail.com',age:24})
+//const task1 = new task({ description: "Clean the house", completed: true });
 
+/*
 task1
   .save()
   .then((res) => {
@@ -26,4 +37,5 @@ task1
   .catch((error) => {
     console.log(error);
   });
-//me.save().then((res)=>{console.log(res);}).catch((error)=>{console.log(error);})
+  */
+me.save().then((res)=>{console.log(res);}).catch((error)=>{console.log(error);})
